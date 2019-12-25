83a25fe4e416ea902a3a2fefcfbc76df04431f0f
Maxli <limingxian525@163.com>
2019-12-25 10:34:15 +0800

    可上传 静态命名 png支持 版本

--- server.js
+++ server.js
@@ -5,20 +5,19 @@ const fileUpload = require('express-fileupload');
 app.use(fileUpload());
 app.post('/upload',function(req,res,next){
     if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).send('No files were uploaded.');
+        res.send('No files were uploaded.');
       }
     
       // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
       let sampleFile = req.files.sampleFile;
     
       // Use the mv() method to place the file somewhere on your server
-      sampleFile.mv('C:/Users/limin/Desktop/1.txt', function(err) {
-        if (err)
-          return res.status(500).send(err);
-    
+      sampleFile.mv('C:/Users/limin/Desktop/1.png', function(err) {
+        if (err){
+          res.send(err);
+        }
         res.send('File uploaded!');
       });
-      next();
 });
 app.use(express.static('www'));
 app.listen(8080);
