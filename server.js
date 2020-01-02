4435a8bad526731b4b3d6635c75714ab7e03beba
Maxli <limingxian525@163.com>
2020-01-02 22:23:59 +0800

    修复了日志无法记录post请求的bug

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express=require('express');
 var app=express();
 const fileUpload = require('express-fileupload');
-//var logger = require('express-logger');
 var morgan = require('morgan');
 var fs=require('fs');
 var path = require('path');
 
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'));
+//创建一个可写流，写入到目录下的access.log文件中        //morgan只能记录“通过”他的请求，放在最后的话post请求被app.post拦截，故应该放在前面
+app.use(morgan(':date[clf] :method', {stream: accessLogStream}));
+//记录日志，形式为short，写入到accessLogStream      //无法记录post请求？？？？？？时间为格林尼治时间？？？？？？
 app.use(fileUpload());
 app.post('/upload',function(req,res,next){
     if (!req.files || Object.keys(req.files).length === 0) {
@@ -21,9 +24,6 @@ app.post('/upload',function(req,res,next){
         res.send('upload successfully!');
       });
 });
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'));
-//创建一个可写流，写入到目录下的access.log文件中
-app.use(morgan(':date[clf]', {stream: accessLogStream}));
-//记录日志，形式为short，写入到accessLogStream      //无法记录post请求？？？？？？时间为格林尼治时间？？？？？？
+
 app.use(express.static('www'));
 app.listen(8080);
