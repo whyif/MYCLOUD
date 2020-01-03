01c3bbb827efe7c94759aba3b480a7dd93bb441b
Maxli <limingxian525@163.com>
2020-01-03 11:19:57 +0800

    修复了服务器重启后日志丢失的bug 新增日志只会记录post请求的功能

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ var morgan = require('morgan');
 var fs=require('fs');
 var path = require('path');
 
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'));
+var accessLogStream = fs.WriteStream(path.join(__dirname, 'upload.log'),{flags:'a'});
+//flags决定了文件操作的方式，默认为“w”，如果文件已经存在会覆盖，“a”表示如果存在就进行追加
 //创建一个可写流，写入到目录下的access.log文件中        //morgan只能记录“通过”他的请求，放在最后的话post请求被app.post拦截，故应该放在前面
-app.use(morgan(':date[clf] :method', {stream: accessLogStream}));
-//记录日志，形式为short，写入到accessLogStream      //无法记录post请求？？？？？？时间为格林尼治时间？？？？？？
+app.post('/upload',morgan(':date[clf] :method', {stream: accessLogStream}));
+//记录日志，形式为short，写入到accessLogStream     
 app.use(fileUpload());
 app.post('/upload',function(req,res,next){
     if (!req.files || Object.keys(req.files).length === 0) {
@@ -22,6 +23,7 @@ app.post('/upload',function(req,res,next){
           res.send(err);
         }
         res.send('upload successfully!');
+        next();
       });
 });
 
